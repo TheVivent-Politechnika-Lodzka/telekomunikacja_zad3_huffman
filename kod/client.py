4a77a912400f10d18f2d665c82955839a7dc07ad
Adam Vivent Kapuściński <a.kapuscinski@thevivent.com>
2021-04-20 11:08:55 +0200

    gotowe ???

--- kod/client.py
+++ kod/client.py
@@ -13,13 +13,16 @@ print('Nawiązywanie połączenia.')
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     print('Utworzono połączenie z {}:{}'.format(HOST,PORT))
-    size = int.from_bytes(s.recv(PACKET_SIZE), "big")
+    tree_size = int.from_bytes(s.recv(PACKET_SIZE), "big") # odbierz rozmiar drzewa
+    # odbierz drzewo
     data = b''
-    while len(data) != size:
+    while len(data) != tree_size:
         data += s.recv(PACKET_SIZE)
     tree = pickle.loads(data)
+    # utwórz obiekt Writera
     writer = HuffmanWriter(filename, tree)
     
+    # odbierz dane
     i = 1
     while data != b'':
         print("Odbieram pakiet nr: {}".format(i), end="\r")
@@ -28,12 +31,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         TRANSMITTED_DATA_SIZE += len(data)
         writer.write(data)
     print('')
+    # zamknij plik
     writer.close()
 
+TREE_SIZE = round(tree_size/1024, 2)
 TRANSMITTED_DATA_SIZE = round(TRANSMITTED_DATA_SIZE/1024, 2)
 FILE_SIZE = round(getsize(filename)/1024, 2)
 COMPRESSION_LEVEL = round((1-TRANSMITTED_DATA_SIZE/FILE_SIZE)*100, 2)
 
+print("Rozmiar drzewa:                     {}kB".format(TREE_SIZE))
 print("Rozmiar przetransmitowanych danych: {}kB".format(TRANSMITTED_DATA_SIZE))
 print("Rozmiar pliku po rozpakowaniu:      {}kB".format(FILE_SIZE))
 print("Poziom kompresji:                   {}%".format(COMPRESSION_LEVEL))
--- kod/server.py
+++ kod/server.py
@@ -12,6 +12,7 @@ filename = input("Jaki plik chcesz przesłać: ")
 reader = HuffmanReader(filename)
 # z obiektu drzewa zrób string
 tree_str = pickle.dumps(reader.tree)
+tree_size = len(tree_str)
 
 TRANSMITTED_DATA_SIZE = 0
 
@@ -22,11 +23,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     with conn:
         print('Nawiązano połączenie z: {}'.format(addr))
-        conn.sendall(len(tree_str).to_bytes(8, byteorder='big'))
+        conn.sendall(len(tree_str).to_bytes(8, byteorder='big')) # wyślij rozmiar drzewa
+        # wyślij drzewo
         while len(tree_str) > 0:
-            data = tree_str[0:512]
+            data = tree_str[0:int(PACKET_SIZE/8)]
             conn.sendall(data)
-            tree_str = tree_str[512:]
+            tree_str = tree_str[int(PACKET_SIZE/8):]
+        # wyślij plik
         i = 1
         while not reader.isEOF():
             print("Wysyłam pakiet nr: {}".format(i), end="\r")
@@ -35,12 +38,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             TRANSMITTED_DATA_SIZE += len(data)
             conn.sendall(data)
         print('')
+        # zamknij plik
         reader.close()
 
+TREE_SIZE = round(tree_size/1024, 2)
 TRANSMITTED_DATA_SIZE = round(TRANSMITTED_DATA_SIZE/1024, 2)
 FILE_SIZE = round(getsize(filename)/1024, 2)
 COMPRESSION_LEVEL = round((1-TRANSMITTED_DATA_SIZE/FILE_SIZE)*100, 2)
 
+print("Rozmiar drzewa:                     {}kB".format(TREE_SIZE))
 print("Rozmiar przetransmitowanych danych: {}kB".format(TRANSMITTED_DATA_SIZE))
 print("Rozmiar pliku przed kompresją:      {}kB".format(FILE_SIZE))
 print("Poziom kompresji:                   {}%".format(COMPRESSION_LEVEL))
